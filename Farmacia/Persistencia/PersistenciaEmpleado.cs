93b11c362c5d14605f3a6ea13e52135dd405c8e4
Edward Rodriguez <edwarderf@gmail.com>
2019-09-21 00:36:01 -0300

    PersistenciaEmpleado Creada

--- Farmacia/Persistencia/PersistenciaEmpleado.cs
+++ Farmacia/Persistencia/PersistenciaEmpleado.cs
@@ -12,6 +12,86 @@ namespace Persistencia
 {
     public class PersistenciaEmpleado
     {
+        public static void Alta(Empleado oEmp)
+        {
+            SqlConnection oConexion = new SqlConnection(Conexion.STR);
+            SqlCommand oComando = new SqlCommand("AltaEmpleado", oConexion);
+            oComando.CommandType = CommandType.StoredProcedure;
+
+            oComando.Parameters.AddWithValue("@nomUsu", oEmp.nomUsu);
+            oComando.Parameters.AddWithValue("@pass", oEmp.pass);
+            oComando.Parameters.AddWithValue("@nombre", oEmp.nombre);
+            oComando.Parameters.AddWithValue("@apellido", oEmp.apellido);
+            oComando.Parameters.AddWithValue("@horaInicio", oEmp.horaInicio);
+            oComando.Parameters.AddWithValue("@horaFinal", oEmp.horaFinal);
+
+            SqlParameter oParametro = new SqlParameter("@Retorno", SqlDbType.Int);
+            oParametro.Direction = ParameterDirection.ReturnValue;
+            oComando.Parameters.Add(oParametro);
+
+            try
+            {
+                oConexion.Open();
+                oComando.ExecuteNonQuery();
+
+                int valReturn = (int)oComando.Parameters["@Retorno"].Value;
+
+                if (valReturn == 1)
+                    throw new Exception("Alta exitosa");
+                if (valReturn == -1)
+                    throw new Exception("Error SQL");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                oConexion.Close();
+            }
+        }
+
+        public static void Modificar(Empleado oEmp)
+        {
+            SqlConnection oConexion = new SqlConnection(Conexion.STR);
+            SqlCommand oComando = new SqlCommand("AltaEmpleado", oConexion);
+            oComando.CommandType = CommandType.StoredProcedure;
+
+            oComando.Parameters.AddWithValue("@nomUsu", oEmp.nomUsu);
+            oComando.Parameters.AddWithValue("@pass", oEmp.pass);
+            oComando.Parameters.AddWithValue("@nombre", oEmp.nombre);
+            oComando.Parameters.AddWithValue("@apellido", oEmp.apellido);
+            oComando.Parameters.AddWithValue("@horaInicio", oEmp.horaInicio);
+            oComando.Parameters.AddWithValue("@horaFinal", oEmp.horaFinal);
+
+            SqlParameter oParametro = new SqlParameter("@Retorno", SqlDbType.Int);
+            oParametro.Direction = ParameterDirection.ReturnValue;
+            oComando.Parameters.Add(oParametro);
+
+            try
+            {
+                oConexion.Open();
+                oComando.ExecuteNonQuery();
+
+                int valReturn = (int)oComando.Parameters["@Retorno"].Value;
+
+                if (valReturn == 1)
+                    throw new Exception("Modificacion exitosa");
+                else if (valReturn == -1)
+                    throw new Exception("Error SQL");
+                else if (valReturn == -2)
+                    throw new Exception("Error - Esta intentando modificar un cliente");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                oConexion.Close();
+            }
+        }
+
         public static void Eliminar(string nomUsu)
         {
             SqlConnection oConexion = new SqlConnection(Conexion.STR);
@@ -44,6 +124,49 @@ namespace Persistencia
             {
                 oConexion.Close();
             }
+        }
+
+        public static Empleado Buscar(string nomUsu)
+        {
+            string pass, nombre, apellido, horaInicio, horaFinal;
+
+            Empleado oEmp = null;
+            SqlDataReader oReader;
+
+            SqlConnection oConexion = new SqlConnection(Conexion.STR);
+            SqlCommand oComando = new SqlCommand("BuscarEmpleado", oConexion);
+            oComando.CommandType = CommandType.StoredProcedure;
+
+            oComando.Parameters.AddWithValue("@nomUsu", nomUsu);
+
+            try
+            {
+                oConexion.Open();
+                oReader = oComando.ExecuteReader();
+
+                if (oReader.Read())
+                {
+                    pass = (string)oReader["pass"];
+                    nombre = (string)oReader["nombre"];
+                    apellido = (string)oReader["apellido"];
+                    horaInicio = (string)oReader["horaInicio"];
+                    horaFinal = (string)oReader["horaFinal"];
+
+                    oEmp = new Empleado(nomUsu, pass, nombre, apellido, horaInicio, horaFinal);
+                }
+
+                oReader.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                oConexion.Close();
+            }
+
+            return oEmp;
         }
     }
 }
