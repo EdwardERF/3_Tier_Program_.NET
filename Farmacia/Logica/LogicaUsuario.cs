c602c3831321fc189b88bfa30410baa6bf1ac32f
Edward Rodriguez <edwarderf@gmail.com>
2019-09-23 17:21:37 -0300

    LogicaEmpleado Creada - MENOS LOGUEO

--- Farmacia/Logica/LogicaUsuario.cs
+++ Farmacia/Logica/LogicaUsuario.cs
@@ -10,5 +10,30 @@ namespace Logica
 {
     public class LogicaUsuario
     {
+        public static void AltaCliente(Cliente oCli)
+        {
+            PersistenciaCliente.Alta(oCli);
+        }
+
+        public static void AltaEmpleado(Empleado oEmp)
+        {
+            PersistenciaEmpleado.Alta(oEmp);
+        }
+
+        public static void ModificarEmpleado(Empleado oEmp)
+        {
+            PersistenciaEmpleado.Modificar(oEmp);
+        }
+
+        public static void EliminarEmpleado(string nomUsu)
+        {
+            PersistenciaEmpleado.Eliminar(nomUsu);
+        }
+
+        public static Empleado BuscarEmpleado(string nomUsu)
+        {
+            Empleado oEmp = PersistenciaEmpleado.Buscar(nomUsu);
+            return oEmp;
+        }
     }
 }
