66bac3ad4c62ee7477c97ee1a939a545a1b3464b
Edward Rodriguez <edwarderf@gmail.com>
2019-09-23 16:32:32 -0300

    LogicaMedicamento Creada

--- Farmacia/Logica/LogicaMedicamento.cs
+++ Farmacia/Logica/LogicaMedicamento.cs
@@ -10,6 +10,35 @@ namespace Logica
 {
     public class LogicaMedicamento
     {
-        
+        public static void Alta(Medicamento oMed)
+        {
+            PersistenciaMedicamento.Alta(oMed);
+        }
+
+        public static void Modificar(Medicamento oMed)
+        {
+            PersistenciaMedicamento.Modificar(oMed);
+        }
+
+        public static void Eliminar(Int64 oRUC, int oCodigo)
+        {
+            PersistenciaMedicamento.Eliminar(oRUC, oCodigo);
+        }
+
+        public static List<Medicamento> Listar()
+        {
+            return PersistenciaMedicamento.Listar();
+        }
+
+        public static List<Medicamento> ListarMedicamentosXFarmaceutica(Int64 oRUC)
+        {
+            return PersistenciaMedicamento.ListarMedicamentosXFarmaceutica(oRUC);
+        }
+
+        public static Medicamento Buscar(Int64 oRUC, int oCodigo)
+        {
+            Medicamento oMed = PersistenciaMedicamento.Buscar(oRUC, oCodigo);
+            return oMed;
+        }
     }
 }
